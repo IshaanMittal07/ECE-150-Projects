d9bc589ceb72104fefbd65a40ae8995470350d22
Ishaan Mittal <ishmittal12@gmail.com>
2025-11-04 00:09:39 -0500

    Hopefully function 3 works idk tho

--- ArrayPointers/project_3.cpp
+++ ArrayPointers/project_3.cpp
@@ -3,8 +3,7 @@
 
 int main();
 
-
-//helper function for geomteric method
+// helper function for geomteric method
 double power(double base, int exp)
 {
     double val = 1.0;
@@ -14,58 +13,94 @@ double power(double base, int exp)
         val *= base;
     }
 
-    return val; 
+    return val;
 }
 
 double *geometric(double a, double ratio, std::size_t cap)
 {
     // returns the address of the array that stores the values ar, ar^2 .... ar^n-1
     double *dynamicArray = new double[cap];
-    double val = 0; 
+    double val = 0;
 
     for (std::size_t i = 0; i < cap; ++i)
     {
         if (i > 0)
         {
             val = a * power(ratio, i);
-            dynamicArray[i] = val; 
-
+            dynamicArray[i] = val;
         }
-        
-        else {
-            dynamicArray[i] = a; 
+
+        else
+        {
+            dynamicArray[i] = a;
         }
     }
-
 
     return dynamicArray;
 }
 
 int main()
 {
-    std::cout << *geometric(2, 2, 3); 
+    std::cout << *geometric(2, 2, 3);
 }
 
 double *cross_correlation(double array0[], std::size_t cap0, double array1[], std::size_t cap1)
 {
-    assert((cap0 + cap1) >= 1); 
-    double *dynamicArray = new double[cap0 + cap1 -1](); //initalize all elements to 0
+    assert((cap0 + cap1) >= 1);
+    double *dynamicArray = new double[cap0 + cap1 - 1](); // initalize all elements to 0
 
-    for (int i = 0; i < cap0; ++i) {
-        for (int j = 0; j < cap1; ++j) {
-            dynamicArray[i + j] += array0[i] * array1[j]; 
+    for (std::size_t i = 0; i < cap0; ++i)
+    {
+        for (std::size_t j = 0; j < cap1; ++j)
+        {
+            dynamicArray[i + j] += array0[i] * array1[j];
         }
     }
 
-    return dynamicArray; 
-
+    return dynamicArray;
 }
 
 std::size_t shift_duplicates(int array[], std::size_t capacity)
 {
+    int unique = 0; 
+    
+    for (int i = 0; i < capacity; ++i) {
+        bool repeat = false; 
+
+        for (int j = 0; j < unique; ++j) {
+            if (array[i] == array[j]) {
+                repeat = true; 
+            }
+        }
+
+        if (repeat == false) {
+            array[unique] = array[i]; 
+            unique++; 
+
+        }
+    }
+
+    return unique; 
+    
 }
 
 void deallocate(double *&ptr, bool is_array, std::size_t capacity = 0)
 {
-    
+    if (is_array == true)
+    {
+        for (std::size_t i = 0; i < capacity; ++i)
+        {
+            ptr[i] = 0;
+        }
+
+        delete[] ptr; // deallocating the memeory of the array
+        ptr = nullptr;
+    }
+
+    else
+    {
+        *ptr = 0;   // sets indivudal value to zero
+        delete ptr; // deallocate single instance
+        ptr = nullptr;
+    }
 }
