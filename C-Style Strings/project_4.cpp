e0857bcd36e02da58ca8a1146ef98f48a0a4d387
Ishaan Mittal <ishmittal12@gmail.com>
2025-11-16 02:00:34 -0500

    Distance function implemented

--- C-Style Strings/project_4.cpp
+++ C-Style Strings/project_4.cpp
@@ -26,81 +26,83 @@ std::size_t length(char const *a)
 
 int compare(char const *str1, char const *str2)
 {
-    int i = 0; 
+    int i = 0;
 
-    while (true) {
-        
+    while (true)
+    {
 
-        if (str1[i] == '\0' && str2[i] == '\0') {
-            return 0; 
+        if (str1[i] == '\0' && str2[i] == '\0')
+        {
+            return 0;
         }
 
-        if (str1[i] != str2[i]) {
-            if (str1[i] < str2[i]) {
-                return -1; 
+        if (str1[i] != str2[i])
+        {
+            if (str1[i] < str2[i])
+            {
+                return -1;
             }
 
-            else {
-                return 1; 
+            else
+            {
+                return 1;
             }
         }
 
-        ++i; 
+        ++i;
 
-        //When the word is like cat and catostrophe the str[3] position is the null character which is always going to smaller than the character it is being compared to, which is why the longer word (catostrophe) comes after the short word (cat)
-
+        // When the word is like cat and catostrophe the str[3] position is the null character which is always going to smaller than the character it is being compared to, which is why the longer word (catostrophe) comes after the short word (cat)
     }
-    
 }
 
-void assign(char *str1, char const *str2) {
-    int i = 0; 
-    while (str2[i] != '\0') {
+void assign(char *str1, char const *str2)
+{
+    int i = 0;
+    while (str2[i] != '\0')
+    {
 
-        str1[i] = str2[i]; 
-        ++i;  
+        str1[i] = str2[i];
+        ++i;
     }
 
-    str1[i] = '\0';  //null characetr 
-
+    str1[i] = '\0'; // null characetr
 }
 
-unsigned int distance( char const *str1, char const *str2) {
-    if (str1 == "") {
-        return length(str2); 
-    } 
-
-    else if(str1[0] == str2[0]) {
-        distance(str1 + 1, str2 + 1); 
+unsigned int distance(char const *str1, char const *str2)
+{
+    if (str1[0] == '\0')
+    {
+        return length(str2);
     }
 
-    else if (str1[0] != str2[0]) {
-
-    }
-    else {
-        return length(str1); 
+    if (str2[0] == '\0')
+    {
+        return length(str1);
     }
 
+    if (str1[0] == str2[0])
+    {
+        return distance(str1 + 1, str2 + 1);
+    }
 
-    int i = 0; 
-
-    
-
+    else
+    {
+        return (1 + std::min(
+                        distance(str1 + 1, str2 + 1),
+                        std::min(distance(str1 + 1, str2), distance(str1, str2 + 1)))); //do this since you can only compare two args in std::min()
+    }
 }
-
 
 int main()
 {
 
-    //Notes: 
-    //Lets say we had char *str1 = brake and char *str2 = str1 +1 
-    //Then eveyrtime u print str2 u would get an output of rake (becuase the pointer will point to the second character 'r')
-    char myString[] = "test"; 
-    char myString2[] = "test2"; 
+    // Notes:
+    // Lets say we had char *str1 = brake and char *str2 = str1 +1
+    // Then eveyrtime u print str2 u would get an output of rake (becuase the pointer will point to the second character 'r')
+    char myString[] = "test";
+    char myString2[] = "test2";
 
-    assign(myString, myString2); 
-    
-
+    assign(myString, myString2);
 
     std::cout << myString << std::endl;
 }
